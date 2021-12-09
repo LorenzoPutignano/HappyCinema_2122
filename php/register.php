7c0421a2fbb2f3003846f7cd052d2fcf72339547
AntoManza <95636309+AntoManza@users.noreply.github.com>
2021-12-09 10:44:48 +0100

    reg adm

--- php/register.php
+++ php/register.php
@@ -1,34 +1,4 @@
 <?php
-    $servername = "localhost";
-    $username_sql = "root";
-    $password_sql ="";
-    $db_name ="test4";
-
-    $user = $_POST["user2"];
-    $password = $_POST["passwd2"];
-
-    try {
-        $conn = new PDO ("mysql:host=".$servername.";dbname=".$db_name, $username_sql, 
-                        $password_sql);
-        $sql_query = "select * from utente where password='" . $password . "';";
-        $result = $conn->query($sql_query);
-        if($result->rowCount() == 1){     //se il conteggio delle righe è maggiore di zero
-            foreach($result as $row) {
-                if($row["password"] == $password) {
-                    echo "[INFO] password già memorizzata!";
-                }
-            }
-        }else {
-            $sql_query_insert = "INSERT INTO utente (user, password) VALUES ('" . $user . "', '" . $password . "');";
-            $result = $conn -> query($sql_query_insert);
-            echo "[INFO] user e password memorizzati con successo!";
-        }
-    } catch(PDOException $exc){
-        echo "connessione error " . $exc->getMessage();
-    };
-?>
-
-<?php
 require 'conn_DB.php';
 $nome = $_POST['name'];
 $cognome = $_POST['surname'];
@@ -36,18 +6,18 @@ $email = $_POST['email'];
 $pass = $_POST['passwd'];
 
 try {
-    $sql_query = "select * from utente where password='" . $password . "';";
+    $sql_query = "select * from utente where password='" . $pass . "';";
     $result = $conn->query($sql_query);
     if($result->rowCount() == 1) {
         foreach($result as $row) {
-            if($row["password"] == $password) {
-                echo "[INFO] password già memorizzata!";
+            if($row["password"] == $pass) {
+                echo "[INFO] utente già memorizzato!";
             }
         }
     else {
         $sql_query = "INSERT INTO utente (nome,cognome,email,password) values ('".$nome."','".$cognome."','".$email."','".$pass."')";
         $result = $conn -> query($sql_query_insert);
-        echo "[INFO] user e password memorizzati con successo!";
+        echo "[INFO] utente memorizzato con successo!";
     }
 } catch (PDOException $exc) {
     echo "error msg: " . $exc->getMessage();
