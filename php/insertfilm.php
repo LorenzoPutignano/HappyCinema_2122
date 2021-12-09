19ebcbf163c2c64eb35c600dfb3fce91031ba443
LorenzoPutignano <lorenzoputignano.work@gmail.com>
2021-12-09 11:33:57 +0100

    ok

--- php/insertfilm.php
+++ php/insertfilm.php
@@ -1,1 +1,20 @@
- 
+<?php
+require 'conn_DB.php';
+$titolo = $_POST['name'];
+$genere = $_POST['surname'];
+$data_uscita = $_POST['email'];
+$orario1 = $_POST['passwd'];
+$orario2 = $_POST['passwd'];
+$orario3 = $_POST['passwd'];
+$descrizione = $_POST['passwd'];
+$duratafilm = $_POST['passwd'];
+
+
+try {
+    $sql_query = "INSERT INTO films (titolo,genere,data_uscita,orario_0,orario_1,orario_2,descrizione,durata_film) values ('".$titolo."','".$genere."','".$data_uscita."','".$orario1."','".$orario2."','".$orario3."','".$descrizione."','".$duratafilm."')";
+    $result = $conn -> query($sql_query_insert);
+    echo "[INFO] Film memorizzato con successo!";
+} catch (PDOException $exc) {
+    echo "error msg: " . $exc->getMessage();
+}
+?>
