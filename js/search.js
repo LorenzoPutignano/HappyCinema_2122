54842d954ce32ed4f3acd0f86899f70e399591b5
FrancescoIntelletto <fra.intelletto@gmail.com>
2021-12-10 11:00:40 +0100

    aggiunto script search

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("#search").click(function(event) {
-        var search = document.getElementById().val();
+        var search = document.getElementById("isearch").val();
         alert(search);
     });
 });
