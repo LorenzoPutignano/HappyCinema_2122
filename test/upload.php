c40fee9329acfd5e6dfc0dd9545e939624f0089b
LorenzoPutignano <lorenzoputignano.work@gmail.com>
2021-12-10 21:20:47 +0100

    funzione salvataggio immagini nelle cartelle del server riuscita!

---
+++ test/upload.php
@@ -0,0 +1,9 @@
+<?php
+    $uploaddir = '../images/';
+    $uploadfile = $uploaddir . basename($_FILES['user_img']['name']);
+    if (move_uploaded_file($_FILES['user_img']['tmp_name'], $uploadfile)) {
+      echo "[INFO] upload ok";
+    } else {
+       echo "[ERR] Upload failed";
+    }
+?>
