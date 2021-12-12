9f7a1b11bca1fbe2e90e6283d1b6671fbe19c461
AntoManza <antonio.03manzari@gmail.com>
2021-12-12 12:50:24 +0100

    trgew

--- php/register.php
+++ php/register.php
@@ -7,18 +7,18 @@ $pass = $_POST['passwd'];
 $pass_cript = hash('sha256',$pass);
 
 try {
-    $sql_query = "select * from utente where email='" . $email . "';";
+    $sql_query = "select email from utente;";
     $result = $conn->query($sql_query);
     if($result->rowCount() == 1) {
         foreach($result as $row) {
             if($row["email"] == $email) {
                 echo "Err";
+            } else {
+                $sql_query = "INSERT INTO utente (nome,cognome,email,password) values ('".$nome."','".$cognome."','".$email."','".$pass_cript."')";
+                $result = $conn -> query($sql_query);
+                echo "ok";
             }
         }    
-    } else {
-    $sql_query = "INSERT INTO utente (nome,cognome,email,password) values ('".$nome."','".$cognome."','".$email."','".$pass_cript."')";
-    $result = $conn -> query($sql_query);
-    echo "ok";
     }
 } catch (PDOException $exc) {
     echo "error msg: " . $exc->getMessage();
