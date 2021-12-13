44c70a5eb5273f4fd830cce09a0037bee6d98707
AntoManza <antonio.03manzari@gmail.com>
2021-12-13 16:00:56 +0100

    risolto table film in remove adm

--- php/films.php
+++ php/films.php
@@ -5,7 +5,7 @@ try {
     $sql_query = "SELECT * FROM films";    
     $result = $conn->query($sql_query);
     foreach($result as $row){
-        echo $row["id_film"].";".$row["titolo"].";".$row["genere"].";".$row["data_uscita"].";".$row["orario_0"].";".$row["orario_1"].";".$row["orario_2"].";".$row["descrizione"].";".$row["durata_film"].$row["immagine"].";".$row["img_film"].";"."|";
+        echo $row["id_film"].";".$row["titolo"].";".$row["genere"].";".$row["data_uscita"].";".$row["orario_0"].";".$row["orario_1"].";".$row["orario_2"].";".$row["descrizione"].";".$row["durata_film"].";".$row["img_film"].";"."|";
     }
 } catch (PDOException $exc) {
     echo "error msg: " . $exc->getMessage();
