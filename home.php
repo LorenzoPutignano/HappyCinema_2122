1cfce2d134856e4595101fc6a3f3806395254f6e
LorenzoPutignano <lorenzoputignano.work@gmail.com>
2021-12-13 16:32:26 +0100

    ok

--- home.php
+++ home.php
@@ -94,17 +94,12 @@
                         </div>
 
                         <div id='removefilm' style='display: none;'>
+                        <br>
                             <div id='Tablefilms' style='display: none;'></div>
-                            <div class='col-cn-1'>
-                                <label for='id' class='form-label'>ID</label>
-                                <input type='number' class='form-control' id='id_film_remove'>
-<<<<<<< HEAD
-                            </div>
-                            <div class='col-cn-1'>
-                                <button type='button' id='remove_bt' class='btn btn-primary'>REMOVE FILM</button>
-=======
-                                <button type='button' id='bt_film' class='btn btn-primary'>REMOVE FILM</button>
->>>>>>> 463a0d5c145e2bba7a16a5fd36742196387e8821
+                            <div class='input-group mb-3'>
+                                <span class='input-group-text' id='basic-addon1'>ID</span>
+                                <input type='number' class='form-control' placeholder='INSERIRE ID FILM DA ELIMINARE' aria-label='ID_FILM' id='id_film_remove' aria-describedby='basic-addon1'>
+                                <button type='button' id='bt_film' class='btn btn-primary'>REMOVE</button>
                             </div>
                     </div>
                         <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p' crossorigin='anonymous'></script>
