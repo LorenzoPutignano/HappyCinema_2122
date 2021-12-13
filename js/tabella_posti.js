eb2381edb54a6c326290d7b0e77e98d5a550a2e4
FrancescoIntelletto <fra.intelletto@gmail.com>
2021-12-13 12:03:08 +0100

    aggiunto immagine della poltrona e corrento link immagine nel file js

--- js/tabella_posti.js
+++ js/tabella_posti.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     for (j = 0; j < 7; j++) {
         append += "<tr>";
         for (i=0 ; i < 7; i++) {
-            append += "<th> <button id = b" + id + " onclick='button(id)'> <img src='poltrona.png' height='40' > </button> </th>";
+            append += "<th> <button id = b" + id + " onclick='button(id)'> <img src='../img/poltrona.png' height='40' > </button> </th>";
             id++;
         }
         append += "</tr>"  
