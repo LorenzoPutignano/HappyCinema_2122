71318ea6b463cbe04c661a1a4230bcb7ab3dffce
LorenzoPutignano <lorenzoputignano.work@gmail.com>
2021-12-14 17:07:41 +0100

    ok

--- index.php
+++ index.php
@@ -35,12 +35,6 @@
 
             }
         });
-
-        function removeItem(sKey, sPath, sDomain) {
-            document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
-        }
-
-
     </script>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
