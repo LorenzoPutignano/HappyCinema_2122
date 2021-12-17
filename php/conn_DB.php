4753a18c1023fff9abe68ea904422365216e7d6f
AntoManza <antonio.03manzari@gmail.com>
2021-12-17 09:52:07 +0100

    Conn_DB.php commentato

--- php/conn_DB.php
+++ php/conn_DB.php
@@ -1,11 +1,12 @@
 <?php
     $servername = "localhost";
     $username_sql = "lorenzo";
-    $password_sql = "Ciao1243!";
+    $password_sql = "Ciao1234!";
     $db_name="cinema";
     try {
         $conn = new PDO ("mysql:host=".$servername.";dbname=".$db_name,$username_sql, $password_sql);
     } catch (PDOException $exc) {
         echo "[CONN_ERR] error msg: " . $exc->getMessage();
     }
+    //file di connessione al database cinema
 ?>
