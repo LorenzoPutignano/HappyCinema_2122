c7808a4ace8c33921444c73b985fe59db1497983
LorenzoPutignano <lorenzoputignano.work@gmail.com>
2021-12-17 10:12:46 +0100

    ok

--- Chose_set.php
+++ Chose_set.php
@@ -13,6 +13,7 @@ if($_SESSION['email'] != null){
                 <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>
                 <!-- Required meta tags -->
                 <meta charset='utf-8'>
+                <link rel='icon' type='image/x-icon' href='./img/Favico.png'>
                 <script src='https://smtpjs.com/v3/smtp.js'></script>
                 <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
                 <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
